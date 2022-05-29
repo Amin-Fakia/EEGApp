cb350ca1e1bfa980193ab941415cdf80b3fb1209
Amin Fakia <ameenofa@gmail.com>
2022-05-29 23:16:37 +0200

    added new features

--- App.py
+++ App.py
@@ -36,8 +36,9 @@ class App(ctK.CTk):
         self.plot_frame = ctK.CTkFrame(master=self, width=1200, height=800,corner_radius=15)
         
         #figures
-        self.fig, self.ax = plt.subplots(figsize=(12,8))
-        
+        self.fig, self.ax = plt.subplots(nrows=2,figsize=(12,8))
+
+
         self.canvas = FigureCanvasTkAgg(self.fig, self.plot_frame)
         self.canvas._tkcanvas.pack(fill=tk.BOTH, expand=1)
         #buttons
@@ -51,12 +52,18 @@ class App(ctK.CTk):
         # entries
         self.lf_entry = ctK.CTkEntry(master=self.editing_frame, placeholder_text="Low Frequency")
         self.hf_entry = ctK.CTkEntry(master=self.editing_frame, placeholder_text="High Frequency")
+        # sliders
+        self.slider = ctK.CTkSlider(master=self.plot_frame, from_=0, to=100, command=self.slider_event)
         
+
+
         self.get_edf_btn.place(relx=0.5,rely=0.5,anchor=tk.CENTER)
+        self.last_t = 0
         self.filepath = ""
         self.edfFile = ""
         self.info = None
         self.raw = None
+        self.frequency = []
     def get_edf(self):
         self.filepath = fd.askopenfilename()
         _, file_extension = os.path.splitext(self.filepath)
@@ -68,14 +75,32 @@ class App(ctK.CTk):
             self.error_label.place(relx=0.5,rely=0.4,anchor=tk.CENTER)
             self.plot_frame.place(relx=0.6,rely=0.48,anchor=tk.CENTER)
             self.reset_data_btn.place(relx=0.5,rely=0.3,anchor=tk.CENTER)
+            self.slider.pack(fill=tk.BOTH,padx=70)
             self.get_plot()
-    def get_plot(self):
-        self.ax.clear()
+    def get_plot(self,min_s=0,max_s=5):
+        self.ax[0].clear()
+        self.ax[1].clear()
         data = self.raw.get_data()
         times = self.raw.times
-        self.ax.plot(times, data[13])
-        self.canvas.draw()
         
+
+        self.ax[0].plot(times, data[13])
+        self.ax[0].axvspan(min_s, max_s, color='red', alpha=0.25)
+        try:
+            ft = np.abs(np.fft.rfft(data[13][int(min_s*300):int(max_s*300)]))
+            frequency = np.linspace(0, 300/2, len(ft))
+            self.ax[1].plot(frequency,ft,color='red',label=(f'{sum(ft):.3f}'))
+            self.ax[1].legend()
+            self.ax[1].set_ylim((0,10e-3))
+        except:
+            pass
+
+        plt.draw()
+        self.canvas.draw()
+    def slider_event(self,value):
+        t = self.last_t * (value/100)
+        self.get_plot(t,t+3)
+
   
     def apply_bandpass(self):
         try:
@@ -107,6 +132,8 @@ class App(ctK.CTk):
     def get_mne_raw(self):
         self.raw = mne.io.read_raw_edf(self.edfFile,preload=True).copy()
         #filename, _ = os.path.splitext(edfFile)
+        self.last_t = self.raw.times[-1]
+        
         keys = ['sfreq','nchan','highpass','lowpass']
         self.info =  [self.raw.info.get(k) for k in keys]
         self.info_frame.place(relx=0.025,rely=0.1, anchor=tk.NW)
